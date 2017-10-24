81318e0e6b22c995ba4b7ba210690df3cd8f43a0
brandonkraybill <bwkrayb@gmail.com>
2017-10-23 23:18:16 -0500

    Changed array to ArrayList, added for loops and while loops.

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -3,6 +3,10 @@ package com.example.android.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -10,27 +14,33 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
-        String[] words = new String[10];
-        words[0] = "one";
-        words[1] = "two";
-        words[2] = "three";
-        words[3] = "four";
-        words[4] = "five";
-        words[5] = "six";
-        words[6] = "seven";
-        words[7] = "eight";
-        words[8] = "nine";
-        words[9] = "ten";
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
 
-        Log.v("NumbersActivity","Word at index 1: " + words[0]);
-        Log.v("NumbersActivity","Word at index 2: " + words[1]);
-        Log.v("NumbersActivity","Word at index 3: " + words[2]);
-        Log.v("NumbersActivity","Word at index 4: " + words[3]);
-        Log.v("NumbersActivity","Word at index 5: " + words[4]);
-        Log.v("NumbersActivity","Word at index 6: " + words[5]);
-        Log.v("NumbersActivity","Word at index 7: " + words[6]);
-        Log.v("NumbersActivity","Word at index 8: " + words[7]);
-        Log.v("NumbersActivity","Word at index 9: " + words[8]);
-        Log.v("NumbersActivity","Word at index 10: " + words[9]);
+        LinearLayout rootView = (LinearLayout)findViewById(R.id.rootView);
+
+//        int index = 0;
+//
+//        while (index < 10) {
+//            TextView wordView = new TextView(this);
+//            wordView.setText(words.get(index));
+//            rootView.addView(wordView);
+//            index++;
+//        };
+
+        for (int indnum = 0; indnum < words.size(); indnum++ ) {
+            TextView wordView = new TextView(this);
+            wordView.setText(words.get(indnum));
+            rootView.addView(wordView);
+        };
     }
 }
