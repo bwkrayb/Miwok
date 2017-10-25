5aa60f4da783e2ecffa6d1b62cac3ba85b88d206
brandonkraybill <bwkrayb@gmail.com>
2017-10-25 13:47:26 -0500

    Changed app to use recycler view for the numbers list.

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -3,7 +3,9 @@ package com.example.android.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -26,8 +28,19 @@ public class NumbersActivity extends AppCompatActivity {
         words.add("nine");
         words.add("ten");
 
-        LinearLayout rootView = (LinearLayout)findViewById(R.id.rootView);
 
+
+
+        ArrayAdapter<String> itemsAdapter =
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, words);
+
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        listView.setAdapter(itemsAdapter);
+
+
+
+//        LinearLayout rootView = (LinearLayout)findViewById(R.id.rootView);
 //        int index = 0;
 //
 //        while (index < 10) {
@@ -37,10 +50,10 @@ public class NumbersActivity extends AppCompatActivity {
 //            index++;
 //        };
 
-        for (int indnum = 0; indnum < words.size(); indnum++ ) {
-            TextView wordView = new TextView(this);
-            wordView.setText(words.get(indnum));
-            rootView.addView(wordView);
-        };
+//        for (int indnum = 0; indnum < words.size(); indnum++ ) {
+//            TextView wordView = new TextView(this);
+//            wordView.setText(words.get(indnum));
+//            rootView.addView(wordView);
+//        };
     }
 }
